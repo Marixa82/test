bc377682d74e9de1d060dd9e2d595aaf9be0a7c5
Maryna Aksakova <121823496+Marixa82@users.noreply.github.com>
2024-08-15 16:24:57 +0300

    add new task

--- js/task-trainer-functions.js
+++ js/task-trainer-functions.js
@@ -1,13 +1,30 @@
 // Task 01
 // Создайте функцию getRandomInt, которая выводит в блок .out-1 случайное целое число от 117 до 132. Все переменные должны быть внутри функции. Запустите эту функцию.
 
+// function getRandomInt () {
+//     const min = 117;
+//     const max = 132;
+//     let randomDigit = Math.floor(min + Math.random() * (max + 1 - min));
+//     console.log(randomDigit)
+// let element = document.querySelector('.out-1');
+// element.textContent = randomDigit;
+// }
+// getRandomInt()
+
+// 2 Напишите функцию getRandomInt2, которая на основе min, max генерирует случайное целое число в этом диапазоне и выводит в out-2. Запустите эту функцию.
 function getRandomInt () {
     const min = 117;
     const max = 132;
     let randomDigit = Math.floor(min + Math.random() * (max + 1 - min));
-    console.log(randomDigit)
+    // console.log(randomDigit)
 let element = document.querySelector('.out-1');
 element.textContent = randomDigit;
+return randomDigit;
 
 }
-getRandomInt();
+function getRandomInt2 () {
+    let getRand = getRandomInt();
+    let el2 = document.querySelector('.out-2')
+    el2.textContent = getRand;
+}
+getRandomInt2();
